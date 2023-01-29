eb82a9095df0b467fbc0cace7bf244878d04e090
wjdghks963 <chsw000@naver.com>
2023-01-29 22:01:39 +0900

    TextField css

--- app/src/main/java/com/example/practice_compose/CreateTodoActivity.kt
+++ app/src/main/java/com/example/practice_compose/CreateTodoActivity.kt
@@ -5,20 +5,23 @@ import android.os.PersistableBundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class CreateTodoActivity: ComponentActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
         setContent {
             CreateTodoView()
@@ -30,12 +33,22 @@ class CreateTodoActivity: ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun CreateTodoView(){
+
+
+    Column(modifier = Modifier.fillMaxSize()
+      ) {
+        EditTextField()
+    }
+
+}
+
+
+@Composable
+fun EditTextField(){
     val text = remember { mutableStateOf("") }
 
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .background(Color.White)) {
-        TextField(value = text.value, onValueChange = {text.value = it})
-    }
+    TextField(value = text.value, onValueChange = {text.value = it}, modifier = Modifier
+        .fillMaxWidth()
+        .border(width = 3.dp, color = Color.Blue, shape = CircleShape), colors = TextFieldDefaults.textFieldColors(backgroundColor = Color.White ,focusedIndicatorColor = Color.Transparent, unfocusedIndicatorColor = Color.Transparent))
 
 }
